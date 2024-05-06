aacd604052b2ebb787733ca4c9cd2c9dea08dcd0
Wojciech Fuśnik <s24618@pjwstk.edu.pl>
2024-05-06 18:36:22 +0200

    18:36,06-05-2024

--- LinqTutorials/Program.cs
+++ LinqTutorials/Program.cs
@@ -7,6 +7,15 @@ namespace LinqTutorials
         static void Main(string[] args)
         {
             var t = LinqTasks.Task1();
+            var t2 = LinqTasks.Task2();
+            var t3 = LinqTasks.Task3();
+            var t4 = LinqTasks.Task4();
+            var t5 = LinqTasks.Task5();
+            var t6 = LinqTasks.Task6();
+            var t7 = LinqTasks.Task7();
+            var t8 = LinqTasks.Task8();
+            var t9 = LinqTasks.Task9();
+            var t10 = LinqTasks.Task10();
 
         }
     }
